abaf6acb11bce68afb51bb3fe1c2f1b66d768d35
renorari <renorari@renorari.net>
2025-01-23 10:36:47 +0900

    sitemapの不具合修正 !rs

--- index.ts
+++ index.ts
@@ -254,7 +254,7 @@ server.get("/sitemap.xml", (req, res) => {
             const lastmod = info.date;
             return `<url><loc>https://renorari.net${encodeURI(url)}</loc><lastmod>${lastmod}</lastmod><priority>0.8</priority></url>`;
         });
-    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join("")}${blogUrls.join()}</urlset>`;
+    const xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls.join("")}${blogUrls.join("")}</urlset>`;
     res.header("Content-Type", "text/xml");
     res.send(xml);
 });
