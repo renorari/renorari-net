01b2eee9de3275a5a763c22b9109a1e95c43866d
renorari <renorari@renorari.net>
2025-03-02 18:55:28 +0900

    🚨 lint

--- eslint.config.js
+++ eslint.config.js
@@ -9,32 +9,37 @@ import pluginRenorari from "./eslint/index.js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    "plugins": {
-      "@stylistic/ts": stylisticTs,
-      "@renorari": pluginRenorari
-    },
-    "rules": {
-      "@/linebreak-style": ["error", "unix"],
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          "argsIgnorePattern": "^_",
-          "caughtErrorsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_"
+    {"files": ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+    {"languageOptions": { "globals": globals.browser }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        "settings": {
+            "react": {
+                "version": "detect"
+            }
+        },
+        "plugins": {
+            "@stylistic/ts": stylisticTs,
+            "@renorari": pluginRenorari
+        },
+        "rules": {
+            "@/linebreak-style": ["error", "unix"],
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    "argsIgnorePattern": "^_",
+                    "caughtErrorsIgnorePattern": "^_",
+                    "destructuredArrayIgnorePattern": "^_",
+                    "varsIgnorePattern": "^_"
+                }
+            ],
+            "@stylistic/ts/indent": ["error", 4],
+            "@stylistic/ts/quotes": ["error", "double"],
+            "@stylistic/ts/semi": ["error", "always"],
+            "@stylistic/ts/comma-dangle": ["error", "never"],
+            "@renorari/no-unquoted-keys": "error"
         }
-      ],
-      "@stylistic/ts/indent": ["error", 4],
-      "@stylistic/ts/quotes": ["error", "double"],
-      "@stylistic/ts/semi": ["error", "always"],
-      "@stylistic/ts/comma-dangle": ["error", "never"],
-      "@renorari/no-unquoted-keys": "error"
     }
-  }
 ];
--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,39 +1,41 @@
-import '../styles.css';
+import "../styles.css";
 
-import type { ReactNode } from 'react';
+import React from "react";
 
-import { Header } from '../components/header';
-import { Footer } from '../components/footer';
+import { Footer } from "../components/footer";
+import { Header } from "../components/header";
+
+import type { ReactNode } from "react";
 
 type RootLayoutProps = { children: ReactNode };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
-  const data = await getData();
+    const data = await getData();
 
-  return (
-    <div className="font-['Nunito']">
-      <meta name="description" content={data.description} />
-      <link rel="icon" type="image/png" href={data.icon} />
-      <Header />
-      <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
-        {children}
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="font-['Nunito']">
+            <meta name="description" content={data.description} />
+            <link rel="icon" type="image/png" href={data.icon} />
+            <Header />
+            <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 const getData = async () => {
-  const data = {
-    description: 'An internet website!',
-    icon: '/images/favicon.png',
-  };
+    const data = {
+        "description": "An internet website!",
+        "icon": "/images/favicon.png"
+    };
 
-  return data;
+    return data;
 };
 
 export const getConfig = async () => {
-  return {
-    render: 'static',
-  } as const;
+    return {
+        "render": "static"
+    } as const;
 };
