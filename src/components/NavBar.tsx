758a510eb3f7bfe27241ced4361ffb7b7358b6f5
renorari <renorari@renorari.net>
2025-05-10 16:39:26 +0900

    ✨ NavBarのリンクを整理

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,17 +6,19 @@ function NavBar() {
     return (
         <nav>
             <h1>
-                <img src="/images/logo.svg" alt="Renorari.net" />
+                <Link to="/">
+                    <img src="/images/logo.svg" alt="Renorari.net" />
+                </Link>
             </h1>
             <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
                 <li>
                     <Link to="/about">About</Link>
                 </li>
                 <li>
                     <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <Link to="/tools">Tools</Link>
                 </li>
             </ul>
         </nav>
