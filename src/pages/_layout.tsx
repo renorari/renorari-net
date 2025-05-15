864b73da068e32f8565e72a6a3cd47240ec65882
renorari <renorari@renorari.net>
2025-05-16 00:48:17 +0900

    ✨ Google Tag Managerのスクリプトを追加

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -22,6 +22,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="canonical" href={canonicalUrl} />
+                {process.env.NODE_ENV === "production" && (
+                    <>
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VLSW5SRFC0"></script>
+                        <script dangerouslySetInnerHTML={{
+                            "__html": `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-VLSW5SRFC0');
+                        `}} />
+                    </>
+                )}
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1265980632516511" crossOrigin="anonymous"></script>
             </head>
             <body>
