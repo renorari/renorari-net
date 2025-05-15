cc40863307d894d6f7b02fb1dc1b03944b588e3d
renorari <renorari@renorari.net>
2025-05-16 08:59:54 +0900

    🐛 404ページからお知らせカードを削除

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 
 import Metadata from "../components/Metadata";
-import Card from "../components/Card";
-import { Link } from "waku";
 
 export default function ContactPage() {
     return (
         <>
             <Metadata title="404 Not Found" noindex />
-
-            <div className="main-width">
-                <Card title={"サイトリニューアルに伴うURL変更のお知らせ"} className="notice">
-                    この度、サイトリニューアルに伴い、URLが変更となりました。<br />
-                    お手数をおかけしますが、ブックマークの変更等をお願いいたします。<br />
-                    なお、旧URLとのリダイレクトは行っておりませんので、何卒ご了承ください。<br />
-                    詳しくは、<Link to="/blog/42">こちら</Link>をご覧ください。
-                </Card>
-            </div>
 
             <main className="center">
                 <h1>
